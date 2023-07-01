172cb83202318233826e0dade4866f5daf7a264e
yamadicu <dicutia@gmail.com>
2023-07-01 15:59:19 +0200

    stampare card

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,43 @@
 <script >
+  import axios from 'axios';
 
+  export default{
+    data(){
+      return{
+        project:[],
+        baseUrl:'http://127.0.0.1:8000'
+      }
+    },
+    mounted(){
+      this.getProject();
+    },
+    methods:{
+      getProject(){
+        axios.get(`${this.baseUrl}/api/projects`)
+        .then(res=>{
+          this.project = res.data.project
+        })
+      }
+    }
+  }
 </script>
 
 <template>
-  <h1>Ciao mondo</h1>
+
+  <div class="container">
+  <h1>ecco i progetti</h1>
+    <div class="row">
+      <div class="col-3 m-2" v-for="(elem,index) in project" :key="index">
+        <div class="card">
+          <img class="card-img-top" :src="`${baseUrl}/storage/${elem.cover_image}`" alt="Title">
+          <div class="card-body">
+            <h4 class="card-title">{{elem.title}}</h4>
+            <p class="card-text">{{elem.content}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
